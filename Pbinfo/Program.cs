036a4843721e0f035be9dff9bf4ea67b0112967b
Paul <Paul@DESKTOP-UHKCL2M>
2024-11-04 21:56:19 +0200

    Add project files.

---
+++ Pbinfo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pbinfo
+{
+    class Program
+    {
+        static void Main()
+        {
+           uint C = uint.Parse(Console.ReadLine());
+
+            uint P = 2 * C;
+            uint G = 2 * P;
+
+            uint animaleInCurte = C + P + G;
+
+            Console.WriteLine(animaleInCurte);
+
+
+        }
+    }
+}
